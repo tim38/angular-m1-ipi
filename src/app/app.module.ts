5cfe24199aca8a532ca1e28919ecf7cf29974e5f
tim38 <timothee.comte@gmail.com>
2021-06-09 22:01:06 +0200

    Ajout JSON CV V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentsModule } from './students/students.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StudentsModule,
   ],
-  providers: [],
+  providers: [ StudentsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
