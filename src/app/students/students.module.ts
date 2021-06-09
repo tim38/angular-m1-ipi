ebe39241e8d292180d1d276a22a95f69239910ec
tim38 <timothee.comte@gmail.com>
2021-06-09 22:02:23 +0200

    Ajout modifs untracked

---
+++ src/app/students/students.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentsComponent } from './students.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[StudentsComponent]
+})
+export class StudentsModule {}
