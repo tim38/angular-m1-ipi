8ad141c026dc5339208dcb45dc20bd9019ec3d95
tim38 <timothee.comte@gmail.com>
2021-07-14 17:07:34 +0200

    Suppression module students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExperienceModule } from './experience/experience.module';
 import { FormationModule } from './formation/formation.module';
-import { StudentsModule } from './students/students.module';
 
 @NgModule({
   declarations: [
@@ -14,11 +13,10 @@ import { StudentsModule } from './students/students.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StudentsModule,
     ExperienceModule,
     FormationModule
   ],
-  providers: [ StudentsModule], //,ExperienceModule,FormationModule
+  //providers: [ StudentsModule], //,ExperienceModule,FormationModule
   bootstrap: [AppComponent]
 })
 export class AppModule { }
